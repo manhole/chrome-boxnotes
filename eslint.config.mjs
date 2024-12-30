bbf3e85d76f426bef2e0512c6b7e2dcc833bd972
manhole <manholex@gmail.com>
2024-12-30 16:05:16 +0900

    chore: introduce typescript-eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// https://typescript-eslint.io/getting-started
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended, {
+  rules: {
+    // https://typescript-eslint.io/rules/no-unused-vars
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+});
